1ac69111be232f12409e8d05a4239f5a71136662
Pic Michel <c.picmichel@gmail.com>
2014-04-26 18:07:35 +0000

    adding a user model and user spec

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140419173545) do
+ActiveRecord::Schema.define(version: 20140426151211) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,14 @@ ActiveRecord::Schema.define(version: 20140419173545) do
     t.string   "title"
     t.text     "content"
     t.string   "author"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "email"
+    t.string   "username"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
--- spec/features/viewing_a_post_spec.rb
+++ spec/features/viewing_a_post_spec.rb
@@ -1,17 +1,9 @@
 require 'spec_helper'
 
 feature 'Viewing a post spec' do
-<<<<<<< HEAD
-  # uncomment the following line when running this feature
   let!(:post) { create(:post) }
 
   scenario do
-    "Please complete creating_posts_spec.rb"
-=======
-  let!(:post) { create(:post) }
-
-  scenario do
->>>>>>> 3678cff38b5360a97298b0b0eadceeefcc7b9dce
     visit posts_path
     click_link post.title
 
